41f07f5ce2c80a91d2fcfe5a95e60122743d7b95
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-03-03 03:01:02 +0900

    feat: generator and locale aetting added

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,19 @@ module Baukis2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.time_zone = 'Tokyo'
+    config.i18n.load_path +=
+      Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_spec false
+      g.view_spec false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
