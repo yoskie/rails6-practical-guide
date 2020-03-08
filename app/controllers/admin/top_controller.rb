5f5ad04998bff583093b41c619d7901de6f3f1c0
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-03-09 07:06:42 +0900

    feat: exception file added

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,2 +1,6 @@
 class Admin::TopController < ApplicationController
+  def index
+    raise IpAddressRejected
+    render action: "index"
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,24 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
   rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
 
   private def set_layout
-    if params[:controller].match(%r{\A(staff|admin|customer)})
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
       "customer"
     end
+  end
+
+  private def rescue403(e)
+    @exception = e
+    render "errors/forbidden", status: 403
   end
 
   private def rescue500(e)
