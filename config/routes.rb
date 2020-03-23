f97b54d000e6d2e8556907e002a9325bcf163a27
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-03-23 22:21:44 +0900

    feat: upto 9-4-1 routing set

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
   namespace :staff, path: "" do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    resource :session, only: [ :create, :destroy ]
+    resource :account, expect: [ :new, :create, :destroy ]
   end
 
   namespace :admin do
     root "top#index"
     get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    resource :session, only: [ :crerate, :destroy ]
     resources :staff_members
   end
 
